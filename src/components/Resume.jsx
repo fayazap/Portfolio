4a29c6ba5e154d838bdc94d973439c30c91c70af
fayazap <fayazap2@gmail.com>
2024-06-28 10:57:30 +0530

    resume change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Resume = () => {
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#contact", { opacity: 1, y: 0, duration: 1, scrollTrigger: {
+      trigger: "#contact",
+      start: "top 90%",
+      end: "bottom 60%",
+      scrub: true,
+    }
+    });
+  });
+
   return (
     <section className='screen-max-width'>
         <div className='screen-max-width'>
         <h1 className="text-3xl font-extrabold sm:text-3xl mt-20 max-sm:mt-10 mb-20 max-sm:mb-10 text-center underline">Resume</h1>
         </div>
-        <div id="contact" className="mt-16 text-center mb-16">
+        <div id="contact" className="mt-16 text-center mb-16 opacity-0 transform translate-y-20">
             <a target="_blank"
-              href="https://drive.google.com/file/d/1Cqhu0IAP44W3tCOu-kNoNEOczdoY5-PK/view?usp=sharing"
+              href="https://drive.google.com/file/d/1ovaVhmjfOOFz-Evz2W1LTVfRlEMtVZ-q/view?usp=sharing"
               className="bg-yellow-400 text-xl text-black px-12 py-3 rounded-lg font-medium mt-4 mb-8 hover:bg-yellow-500 transition-all"
             >
               Download Resume
